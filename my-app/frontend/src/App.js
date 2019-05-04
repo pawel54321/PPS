d186acf5c0a551cacd8fbd975140176edd5e5bc9
pawel54321 <pawlo.pw36@gmail.com>
2019-05-04 14:57:44 +0200

    Alert Repair

--- my-app/frontend/src/App.js
+++ my-app/frontend/src/App.js
@@ -9,6 +9,15 @@ import Register from './Components/Register';
 
 import Alert from 'react-s-alert';
 import 'react-s-alert/dist/s-alert-default.css';
+// optional - you can choose the effect you want
+import 'react-s-alert/dist/s-alert-css-effects/slide.css';
+import 'react-s-alert/dist/s-alert-css-effects/scale.css';
+import 'react-s-alert/dist/s-alert-css-effects/bouncyflip.css';
+import 'react-s-alert/dist/s-alert-css-effects/flip.css';
+import 'react-s-alert/dist/s-alert-css-effects/genie.css';
+import 'react-s-alert/dist/s-alert-css-effects/jelly.css';
+import 'react-s-alert/dist/s-alert-css-effects/stackslide.css';
+
 class App extends Component {
 
     /*
