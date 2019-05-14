6106fc830fea3f2efcf22740dfffeebfe0186086
Grzegorz Żak <37216924+gpzak@users.noreply.github.com>
2019-05-14 16:16:35 +0200

    Create Post.js

---
+++ my-app/frontend/src/Components/Post.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Post</p>
+            </div>
+        );
+    }
+}
+
+export default Post;
