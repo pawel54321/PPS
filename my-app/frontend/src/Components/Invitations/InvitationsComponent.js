5dc4ea2bb303f07f63dd791091285b534be7a835
pawel54321 <pawlo.pw36@gmail.com>
2019-05-18 02:21:25 +0200

    Update InvitationsComponent.js

--- my-app/frontend/src/Components/Invitations/InvitationsComponent.js
+++ my-app/frontend/src/Components/Invitations/InvitationsComponent.js
@@ -27,17 +27,37 @@ class InvitationsComponent extends Component {
                 isOpen={this.state.isPaneOpenLeft}
                 title='Zarządzanie zaproszeniami'
                 from='left'
-                width='370px'
+                width='400px'
                 onRequestClose={() => this.setState({ isPaneOpenLeft: false })}>
-                <center>Wysłane zaproszenia:</center>
+                <center><b>Wysłane prośby o dołaczenie do grup:</b></center>
                 <br/>
-
-                <center>Oczekujące zaproszenia:</center>
-
+                &#8592; Twoje zapytanie do grupy 'AAA' oczekuje na akceptacje... [USER]<br />
+                <br />
+                <center><b>Otrzymane prośby o dołączenie do grup:</b></center>
+                <br />
+                &#8592; Zaproszono Cię do grupy 'AAA' [USER]<br />
+                &#8594; Użytkownik 'A' chce dołączyć do twojej grupy 'AAA' [MOD]
                 <br />
             </SlidingPane>
         </div>;
     }
+    /*
+                <center>Wysłane prośby o dołaczenie do grup:</center>
+                <br/>
+                Twoje zapytanie do grupy 'AAA' oczekuje na akceptacje... [USER]
+                //Twoje zapytanie do grupy 'AAA' zostało odrzucone!
+                //Twoje zapytanie do grupy 'AAA' zostało zaakceptowane!
+                <br />
+                <center>Otrzymane prośby o dołączenie do grup:</center>
+                <br />
+                Zaproszono Cię do grupy 'AAA' [USER]
+                Użytkownik 'A' chce dołączyć do twojej grupy 'AAA' //Dostaje powiadomienie ze ktos chce dolaczy do grupy Akceptacja/Odrzucenie [MOD]
+
+                //Moderator ma tabele kogo moze zaprosic i jak zaprosi to znika z listy AND stan="Oczekujacy" nie pokazuje [MOD]
+                <br />
+
+
+    */
 }
 
 export default InvitationsComponent;
