320f9b1e43ad407ad246c851040c78b83e44ddfb
pawel54321 <pawlo.pw36@gmail.com>
2019-05-20 20:10:03 +0200

    Update HomePage.js

--- my-app/frontend/src/Components/HomePage.js
+++ my-app/frontend/src/Components/HomePage.js
@@ -5,45 +5,97 @@ import Post from './PostsForGroups/Post';
 
 import {Input } from 'react-chat-elements';
 import { Button } from 'react-chat-elements'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class HomePage extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            token: null,
+        }
+
+       
+
+    }
+    componentDidMount() {
+        this.zwrocenieCzyZalogowany();    
+    }
+
+
+    zwrocenieCzyZalogowany = async () => {
+        const token = await axios.post('http://localhost:5000/ReadToken', {
+            token: localStorage.getItem('token')
+        });
+
+        this.setState({
+            token: token
+        });
+    }
 
     render() {
-      
-        return (
-            <div>
-                <Row>
-                    <Col md={3}>
-                        <GroupList />
-                    </Col>
-                    <Col md={9}>
-                        {/*zawartość grupy*/}
-                 
-                        <Post />
 
-                        <Input
-                           
-                            placeholder="Skomentuj..."
-                            multiline={false}                           
-                            rightButtons={
-                                <Button
-                                    color='white'
-                                    backgroundColor='black'
-                                    text='Wyślij' />
-                            }
+        if (!this.state.token) {
+
+            return (
+                <div>
+                    <br/>
+                    <Col sm='12' md={{ size: 12, offset: 0 }}>
+                        <Row>
+
+                            <h4 style={{ textAlign: 'center', width: '100%' }}>
+                                <br /> <br /> <br /> <br /> <br />
+                                <h1>Witamy!</h1><br />
+                                Do prawidłowego korzystania z serwisu wymagane jest posiadanie konta!<br />
+                                Zarejestruj się, klikając w odpowiedni przycisk w panelu nawigacyjnym lub <Link to="/register">Tutaj</Link>.<br /><br />
+                                <h3>Dziękujemy za korzystanie z serwisu i życzymy udanych integracji!</h3></h4>
+                        </Row>
+                    </Col>
+                </div>
+            );
+
+        }
+        else
+        {
+
+
+            return (
+                <div>
+
+                    <Row>
+                        <Col md={3}>
+                            <GroupList />
+                        </Col>
+                        <Col md={9}>
+                            {/*zawartość grupy*/}
+
+                            <Post />
+
+                            <Input
+
+                                placeholder="Skomentuj..."
+                                multiline={false}
+                                rightButtons={
+                                    <Button
+                                        color='white'
+                                        backgroundColor='black'
+                                        text='Wyślij' />
+                                }
 
                             /*OnKeyPress={(e) => { this.refs.input.clear() }}*/ />
 
-                        {/* emitonki*/}
-                    </Col>
-                </Row>
-                {/*
+                            {/* emitonki*/}
+                        </Col>
+                    </Row>
+                    {/*
                  jesli zalogowany to po lewej lista grup (panel) ktore 'ma' i moze w nie klikac i sie pojawia mozliwosc po prawej stronie dodania postu/komentarza do tej grupy
-                 */}    
-                <br />  <br />  <br />
-            </div>
-        );
+                 */}
+                    <br />  <br />  <br />
+                </div>
+            );
+        }
     }
 }
 
