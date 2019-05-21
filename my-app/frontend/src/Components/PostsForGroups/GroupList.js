fb19a6274e6e8b9b32913803affcc5d95039cf64
Grzegorz Żak <37216924+gpzak@users.noreply.github.com>
2019-05-22 00:35:22 +0200

    Update

--- my-app/frontend/src/Components/PostsForGroups/GroupList.js
+++ my-app/frontend/src/Components/PostsForGroups/GroupList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import {Col, Row, TabContent, ListGroup, NavLink, ListGroupItem } from 'reactstrap';
+import MyGroup from './MyGroup';
+import axios from 'axios';
 
 class GroupList extends Component {
 
@@ -9,15 +11,34 @@ class GroupList extends Component {
             groups: [],
             user: null
         }
+        this.zwrocenieGrup();
     }
 
-
+    zwrocenieGrup = async () => {
+        const token = await axios.post('http://localhost:5000/ReadToken', {
+            token: localStorage.getItem('token')
+        });
+        const groups = await axios.post('http://localhost:5000/Grupa/Wyswietl/DanyLogin/User', {
+            id: token.data.user.id
+        });
+        //console.log(groups);
+        this.setState({
+            groups: groups.data.wyswietl
+        });
+    }
 
     render() {
         return (
-            <ListGroup>
-                <ListGroupItem></ListGroupItem>
-            </ListGroup>
+            <div>
+                <br />
+                <center><b>Moje grupy</b></center>
+                <br />
+                <div style={{ "overflow-y": "auto", "overflow-x": "hidden", "height": "700px", "scrollbar-width": "none" }}>
+                    <ListGroup>
+                        {this.state.groups.map(dane => <MyGroup info={dane} />)}
+                    </ListGroup>
+                </div>
+            </div>
         );
     }
 }
